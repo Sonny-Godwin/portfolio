bb7c75c7acb43d9c836316eb5661eaa941884361
Sonny Godwin <srgodwin3@yahoo.com>
2015-12-13 19:28:58 +0000

    Basic Website Complete

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,5 +5,10 @@ class PagesControllerTest < ActionController::TestCase
     get :home
     assert_response :success
   end
+  
+  test "should get home" do
+    get :about
+    assert_response :success
+  end
 
 end
